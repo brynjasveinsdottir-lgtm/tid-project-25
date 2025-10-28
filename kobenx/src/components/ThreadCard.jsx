e0922baa58cc2d2eac2ca9a5e0a2cbd66a33d0a3
josefine-s-j <js.joh117@gmail.com>
2025-10-28 21:59:58 +0100

    Added like, bookmark and repost button.
    Added like, bookmark and repost button. Changed threadText classname.

--- kobenx/src/components/ThreadCard.jsx
+++ kobenx/src/components/ThreadCard.jsx
@@ -1,16 +1,51 @@
-import React from "react";
+import { useState } from "react";
 import "./CardStyle.css";
-import Ellipse from "/src/assets/Ellipse.png";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
+import BookmarkOutlinedIcon from "@mui/icons-material/BookmarkOutlined";
+import LoopOutlinedIcon from '@mui/icons-material/LoopOutlined';
 import UserDisplay from "./UserDisplay";
 
+
 function ThreadCard({ thread }) {
+
+const [liked, setLiked] = useState(false);
+const [bookmarked, setBookmarked] = useState(false);
+const [repostet, setRepostet] = useState(false);
+
+const handleLike = () => setLiked(!liked);
+const handleBookmark = () => setBookmarked(!bookmarked);
+const handleRepost = () => setRepostet(!repostet);
+
+
+
   return (
     <article className="card">
         <UserDisplay userInfo={thread.author}/>
       <div className="threadContent">
-        <p className="thread_text">{thread.text}</p>
+        <p className="threadText">{thread.text}</p>
+        </div>
+      
+      {/* Like Button */}
+      <div className="threadActions">
+      <button onClick={handleLike} className="likeButton">
+      {liked? <FavoriteIcon color="error" /> : <FavoriteBorderIcon/>}
+      </button>
+      
+      {/* Bookmark Button */}
+      <button onClick={handleBookmark} className="bookmarkButton">
+      {bookmarked? <BookmarkOutlinedIcon color="error" /> : <BookmarkBorderOutlinedIcon/>}
+      </button>
+
+      {/* Repost Button */}
+      <button onClick={handleRepost} className="repostButton">
+     {repostet ? <LoopOutlinedIcon sx={{ color: "#444" }} /> : <LoopOutlinedIcon sx={{ color: "#e53935" }} />}
+      </button>
+
+
       </div>
+
     </article>
   );
 }
