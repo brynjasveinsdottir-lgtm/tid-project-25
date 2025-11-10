b051d017caa0aa6febf975330fd7c30b87798ab8
brsv <brynjasveinsdottir@gmail.com>
2025-11-10 10:31:45 +0100

    testing all post types
    not ready to merge, more a testing of how we might work with mixed feeds and filters. Filters are not applied yet though.

--- kobenx/src/Pages/HomePage.jsx
+++ kobenx/src/Pages/HomePage.jsx
@@ -5,17 +5,45 @@ import FilterChip from "../components/FilterChip";
 
 import Filters from "../components/Filters";
 
+import { useState, useEffect } from "react";
+import Parse from "parse";
+import EventCard from "../components/EventCard";
+import Post from "../components/PostTemplate";
+
+import "/src/assets/Manrope.ttf";
+import "/src/index.css";
+import "./PageStyle.css";
+
 export default function Home() {
   const filters = ["Social", "Food", "Housing", "Language", "Transport"];
+  const [posts, setPosts] = useState([]);
+
+  // Get all posts that have category 'Event' from class 'Posts' in database using Parse
+  useEffect(() => {
+    async function getPosts() {
+      const Posts = Parse.Object.extend("Posts");
+      const query = new Parse.Query(Posts);
+      /* query.greaterThanOrEqualTo('eventTime', 0) */
+      query.include("author");
+      const results = await query.find();
+      setPosts(results);
+    }
+    getPosts();
+  }, []);
+
   return (
     <div className="page-structure">
       <h1 className="page-title">Home</h1>
       <TextField />
 
       <FilterChip> Testing single chip </FilterChip>
-      <Filters  filterList = {filters} />
 
-
+      <Filters filterList={filters} />
+      <div>
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
     </div>
   );
 }
--- kobenx/src/components/UserDisplay.jsx
+++ kobenx/src/components/UserDisplay.jsx
@@ -1,8 +1,66 @@
-import * as React from 'react';
+import React, { useState } from "react"
 import './UserDisplay.css';
 import Avatar from './Avatar';
 
-export default function UserDisplay({userInfo}) {
+export default function UserDisplay({userInfo, userInfoParse, time}) {
+
+  
+
+  if (userInfoParse) {
+    const profilePic = userInfo? userInfo.get("profilePicture"): null
+    const profilePicUrl = profilePic? profilePic?.url?.(): null
+
+    //time since moved function
+    const timeSinceMoved = () => {
+  
+      const movedDate = new Date(userInfoParse.get("dateMovedToChp"));
+      const now = new Date();
+    
+      // Calculate differences
+      const diffTime = now - movedDate;
+      const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    
+      let value, unit;
+    
+      if (diffDays < 30) {
+        value = diffDays;
+        unit = "day";
+      } else if (diffDays < 365) {
+        value = Math.floor(diffDays / 30);
+        unit = "month";
+      } else {
+        value = Math.floor(diffDays / 365);
+        unit = "year";
+      }
+    
+      const multiple = value !== 1 ? "s" : "";
+      return `${movedDate}`;
+    };
+
+    return (
+    <div className='user_display'>
+        
+    <Avatar alt={userInfoParse.get("firstName")? userInfoParse.get("firstName"): "XX"} src={profilePicUrl} />    
+      <div className='column'>
+
+        <div className='row'>
+        <p className="username"> {userInfoParse.get("firstName")? userInfoParse.get("firstName"): "FirstName"} {userInfoParse? userInfoParse.get("lastName"): "User name"} </p>
+            <div className='tag'> {timeSinceMoved()} </div> {/* How long the user has lived in CPH */}
+            <p className= "subtle">• {time}</p>  {/* Timestamp */}
+
+        </div>
+
+            <p className= "subtle">{userInfoParse.get("occupation")? userInfoParse.get("occupation"): "bio"}</p>
+      </div> 
+
+    </div> 
+    );
+
+  }
+
+
+
+  else {
   return (
 
     <div className='user_display'>
@@ -22,4 +80,4 @@ export default function UserDisplay({userInfo}) {
 
     </div>
   );
-}
+} }
