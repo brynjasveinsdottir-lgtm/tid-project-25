c5999bd308404853cd88b9e54a2dd915b15d939c
FrederikSolbo <frederik.solbo@me.com>
2025-11-28 11:55:07 +0100

    Revert "Event Section Fix"
    This reverts commit 70d208f29dd85e78f9f480a0ca8226a439eac81c.

--- kobenx/src/components/EventSections.jsx
+++ kobenx/src/components/EventSections.jsx
@@ -4,39 +4,18 @@ import Parse from "parse"
 import EventCard from '/src/components/EventCard.jsx'
 import '/src/assets/Manrope.ttf'
 import '/src/index.css'
-import './PageStyle.css'
+import '/src/Pages/PageStyle.css'
 
-export default function EventSection() {
-    
-    // Filter based on 'Music' and 'Food'. Will update to add more for more event categories.
-    const music = events.filter(event =>
-        event.get("eventCategory") === 'Music'
-    );
-    const food = events.filter(event =>
-        event.get("eventCategory") === 'Food'
-    );
+export default function EventSection({ category, events }) {
 
-    // Structuring the events page
-    // Remove individual categories
     return (
-        <div className="page-structure">
-            <h1 className="page-title">Events</h1>
+        <div className="category-section">
             <h2 className="category-header">
-                Music
+                {category}
             </h2>
             <div className='category-row'>
                 <div className='event-row'>
-                    {music.map((post) => (
-                        <EventCard key = {post.id} event = {post} />
-                    ))}
-                </div>
-            </div>
-           <h2 className="category-header">
-                Food
-            </h2>
-            <div className='category-row'>
-                <div className='event-row'>
-                    {food.map((post) => (
+                    {events.map((post) => (
                         <EventCard key = {post.id} event = {post} />
                     ))}
                 </div>
