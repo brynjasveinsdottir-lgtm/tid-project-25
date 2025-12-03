f4dbc3cfda6d03093ef377989e249f3a897a4c82
FrederikSolbo <frederik.solbo@me.com>
2025-12-03 11:12:58 +0100

    Login flow fix
    Added .then() to login flow to handle errors.
    Username and password fields are now red after error until information is typed into fields again.

--- kobenx/src/components/LoginFlow.jsx
+++ kobenx/src/components/LoginFlow.jsx
@@ -5,6 +5,8 @@ import { useState } from "react"
 export default function LogIn() {
 
     const [isButtonDisabled, setIsButtonDisabled] = useState(true)
+    const [failedUsername, setFailedUsername] = useState(false)
+    const [failedPassword, setFailedPassword] = useState(false)
     const [isFilled, setIsFilled] = useState({
         username: false,
         password: false
@@ -13,6 +15,12 @@ export default function LogIn() {
     function handleFilled(e) {
         const newValue = e.target.value
         const fieldName = e.target.name
+
+        if (e.target.name === 'username') {
+            setFailedUsername(false)
+        } else {
+            setFailedPassword(false)
+        }
 
         setIsFilled(prev => {
             const updated = {
@@ -33,8 +41,17 @@ export default function LogIn() {
     async function login(formData) {
         const username1 = formData.get('username')
         const password1 = formData.get('password')
-        const user = await Parse.User.logIn(`${username1}`, `${password1}`)
-        window.location.reload()
+        const user = await Parse.User.logIn(`${username1}`, `${password1}`).then(
+            (newObj) => {
+                window.location.reload()
+            },
+            (error) => {
+                alert(error.message)
+                setFailedUsername(true)
+                setFailedPassword(true)
+            }
+        )
+        
     }
 
     return (
@@ -42,13 +59,19 @@ export default function LogIn() {
             <form className="form-box" action={login}>
                 <div className="input-column login">
                     <label htmlFor='username'> Username </label>
-                    <input type='username' placeholder="Username" name='username' className='inputfield' onInput={handleFilled}/>
+                    <input type='username' placeholder="Username" name='username' className={`inputfield ${failedUsername ? 'failed' : ''}`} onInput={handleFilled}/>
                 </div>
                 <div className="input-column login">
                     <label htmlFor='password'> Password </label>
-                    <input type='password' placeholder="Password" name='password' className="inputfield" onInput={handleFilled}/>
+                    <input type='password' placeholder="Password" name='password' className={`inputfield ${failedPassword ? 'failed' : ''}`} onInput={handleFilled}/>
                 </div>
-                <button title={isButtonDisabled ? 'Enter Username & Password first' : 'Click me to log in!'} type="submit" value='submit' className={isButtonDisabled ? 'disabledButton' : 'enabledButton'} disabled={isButtonDisabled}>Log-In</button>
+                <button
+                    title={isButtonDisabled ? 'Enter Username & Password first' : 'Click me to log in!'}
+                    type="submit" value='submit'
+                    className={isButtonDisabled ? 'disabledButton' : 'enabledButton'}
+                    disabled={isButtonDisabled}>
+                    Log-In
+                </button>
             </form>
         </div>
     );
