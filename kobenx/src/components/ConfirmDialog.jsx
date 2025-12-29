260527b4fe3b8191e4724ff88c7c4bbdc711d288
brsv <brynjasveinsdottir@gmail.com>
2025-12-29 18:49:13 +0000

    Update ConfirmDialog.jsx

--- kobenx/src/components/ConfirmDialog.jsx
+++ kobenx/src/components/ConfirmDialog.jsx
@@ -20,6 +20,7 @@ export default function ConfirmDialog({
       title={title}
       closeOnOutsideClick={false}
       isDismissible={false}
+      role="alertdialog"
       size="sm"
     >
       <div className="dialog-content">
