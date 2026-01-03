86b04245c069e2b42c7e59cc88d2db86adfbd14c
josefine-s-j <js.joh117@gmail.com>
2026-01-03 14:01:53 +0100

    Made back button from OpenThread

--- kobenx/src/Pages/ThreadOpen.jsx
+++ kobenx/src/Pages/ThreadOpen.jsx
@@ -1,14 +1,17 @@
 import { useState, useEffect } from "react";
 import Parse from "parse";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import PostTemplate from "../components/PostTemplate";
 import AddComment from "../components/AddComment";
 import CommentList from "../components/CommentList";
 import "./PageStyle.css";
 import "/src/assets/Manrope.ttf";
+import Button from "../components/Button";
 
 export default function ThreadOpen() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const [post, setPost] = useState(null);
   const [comments, setComments] = useState([]);
@@ -49,13 +52,24 @@ export default function ThreadOpen() {
     if (post) fetchComments();
   }, [post]);
 
-  if (!post) return;
-  <div className="page-structure">Loading thread...</div>;
+  if (!post) {
+    return <div className="page-structure">Loading thread...</div>;
+  }
 
   return (
     <div className="page-structure">
       <h1 className="page-title">Thread</h1>
-
+      <Button
+        className="back-button"
+        type="button"
+        variant="secondary"
+        isRounded
+        onClick={() => {
+          navigate("/threads");
+        }}
+      >
+        Back
+      </Button>
       <div className="thread-open-container">
         <PostTemplate post={post} />
         <AddComment post={post} onCommentAdded={fetchComments} />
