12b25cc6b896467fcf6ba1b59a074c3a32e09d12
FrederikSolbo <frederik.solbo@me.com>
2026-02-26 19:27:49 +0100

    Ngrok

--- kobenx/vite.config.js
+++ kobenx/vite.config.js
@@ -1,19 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
-  
-  // Tell Vite that when we refer to "parse" we talk about the minified version
+
   resolve: {
     alias: {
       parse: "parse/dist/parse.min.js",
     },
-    
-  // Beter performance during development 
+  },
+
+  // Better performance during development
   optimizeDeps: {
     include: ["parse"],
   },
-  }
+
+  // 👇 Add this
+  server: {
+    host: true,
+    allowedHosts: true,
+  },
 })
